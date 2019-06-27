210f57dbf2f6edadbd4b2785f83aaa2e7c4ab6a9
Emika Ijuin <emikaijuin@gmail.com>
2019-06-27 14:56:37 -0400

    generated user model - first commit

---
+++ lib/phoenix_app/user.ex
@@ -0,0 +1,22 @@
+defmodule PhoenixApp.User do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "users" do
+    field :confirmation_token, :string
+    field :email, :string
+    field :first_name, :string
+    field :last_name, :string
+    field :password_digest, :string
+    field :remember_token, :string
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(user, attrs) do
+    user
+    |> cast(attrs, [:first_name, :last_name, :email, :password_digest, :confirmation_token, :remember_token])
+    |> validate_required([:first_name, :last_name, :email, :password_digest, :confirmation_token, :remember_token])
+  end
+end
