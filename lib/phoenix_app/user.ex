d0ac12bc02e10394e3c7ac4941295696b5590fe3
Emika Ijuin <emikaijuin@gmail.com>
2019-06-28 11:36:30 -0400

    validate presence of password and store pw hash

--- lib/phoenix_app/user.ex
+++ lib/phoenix_app/user.ex
@@ -22,17 +22,27 @@ defmodule PhoenixApp.User do
       :first_name,
       :last_name,
       :email,
-      :password_digest,
-      :confirmation_token,
-      :remember_token
+      :password
     ])
     |> validate_required([
       :first_name,
       :last_name,
       :email,
-      :password_digest,
-      :confirmation_token,
-      :remember_token
+      :password
     ])
+    |> unsafe_validate_unique(:email, Repo)
+    |> put_password_hash()
+  end
+
+  defp put_password_hash(changeset) do
+    IO.inspect(changeset)
+
+    case changeset do
+      %Ecto.Changeset{valid?: true, changes: %{password: pass}} ->
+        put_change(changeset, :password_digest, Comeonin.Bcrypt.hashpwsalt(pass))
+
+      _ ->
+        changeset
+    end
   end
 end
