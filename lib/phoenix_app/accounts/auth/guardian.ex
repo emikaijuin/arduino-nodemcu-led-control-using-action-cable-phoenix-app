23c4c7a68b50090a9dd7a14176b098b7cddbbe85
Emika Ijuin <emikaijuin@gmail.com>
2019-06-28 11:17:53 -0400

    add additional callbacks

--- lib/phoenix_app/accounts/auth/guardian.ex
+++ lib/phoenix_app/accounts/auth/guardian.ex
@@ -4,16 +4,24 @@ defmodule PhoenixApp.Auth.Guardian do
   use Guardian, opt_app: :phoenix_app
 
   alias PhoenixApp.Accounts
+  alias PhoenixApp.User
 
   def subject_for_token(user, _claims) do
     sub = to_string(user.id)
     {:ok, sub}
   end
 
+  def subject_for_token(_, _) do
+    {:error, :reason_for_error}
+  end
+
   def resource_from_claims(claims) do
     id = claims["sub"]
-    user = Accounts.get_user(id)
+    user = PhoenixApp.get(User, id)
     {:ok, user}
   end
-end
 
+  def resource_from_claims(_claims) do
+    {:error, :reason_for_error}
+  end
+end
